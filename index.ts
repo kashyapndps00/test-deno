0931a4fe84f7f7ab3dae725f0ab95c15f0ea04e7
Kashyap Sharma <98746601+kashyapndps00@users.noreply.github.com>
2023-02-06 00:43:04 +0530

    Fixed : not fetching env by adding Deno.env

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import { Bot } from "https://deno.land/x/grammy@v1.14.1/mod.ts"
 import { load } from "https://deno.land/std/dotenv/mod.ts";
 
-const env = await load();
-if(!env["BOT_TOKEN"]) throw Error("BotError: Token is not set");
-const bot = new Bot(env["BOT_TOKEN"])
+// const env = await load();
+const token = Deno.env.get('BOT_TOKEN');
+if(!token) throw Error("BotError: Token is not set");
+const bot = new Bot(token)
 
 bot.command("start",ctx=>{
     ctx.reply(`<b>Hello ${ctx.from?.first_name}</b>, This is a test bot made by @Kashyap_back using typescript, deno and grammY.\nIf you find any bugs please send it to me so that I can imporve my bot.`,{
